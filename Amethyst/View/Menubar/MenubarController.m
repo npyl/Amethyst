77388c0495239fedcdec5ab8b486ecd7755897d0
npyl <pylarinosnick@gmail.com>
2020-04-28 19:10:45 +0300

    Add some TODOs

--- Amethyst/View/Menubar/MenubarController.m
+++ Amethyst/View/Menubar/MenubarController.m
@@ -15,12 +15,17 @@
     self = [super initWithWindow:window];
     if (self) {
         _offset = 0;
+
+        // TODO: Register to listen for specific keys e.g. cmd+r: run (then this will use spotlight on the background for search & suggestions)
     }
     return self;
 }
 + (instancetype)forMenubar:(Menubar *)menubar {
     return [[super alloc] initWithWindow:menubar];
 }
+
+// TODO: Patch -push to work for any kind of menubar objects and introduce the text view
+// TODO: make amethyst take into account the size of the menubar (=> new windows need to stop overlapping with the menubar)
 - (void)push {
     /* Create button */
     MacButton *button = [[MacButton alloc] init];
